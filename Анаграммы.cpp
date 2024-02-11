34a70d6d6be0b88c6d4a1f81058b59f1ef54c994
Aleksandr <aleksandrozerov270785@gmail.com>
2024-02-11 18:19:42 +0300

    Добавьте файлы проекта.

---
+++ Анаграммы.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool Anograms(string s, string j) {
+	sort(s.begin(), s.end());
+	sort(j.begin(), j.end());
+	return (s == j);
+}
+
+int main(){
+	cout << boolalpha;
+	cout << Anograms("abcd", "dcab") << endl;
+	cout << Anograms("cat", "tad")<< endl;
+	
+
+}
